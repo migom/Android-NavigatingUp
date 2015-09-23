07246fba8065862c2d1d2d9cf521fecab9a9217c
migom <virspace@gmail.com>
2015-09-23 15:27:50 +0900

    NavigatingUp 최초 생성.

---
+++ java/com/google/android/sample/navigatingup/ParentActivity.java
@@ -0,0 +1,39 @@
+package com.google.android.sample.navigatingup;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ParentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_parent, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_call_child:
+                startActivity(new Intent(this, ChildActivity.class));
+                break;
+            case R.id.action_settings:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
